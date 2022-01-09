d388dfd5343b215af26bcd487b1b6e712a827354
KRamBalaji <krambalaji27@gmail.com>
2022-01-09 20:35:54 +0530

    created a function to count words and added a timer using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
   const [text,setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   function handleChange(e) {
     const {value} = e.target;
     setText(value);
   }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ")
+    return wordsArr.filter(word => word !== "").length
+  }
+
+  useEffect(() => {
+    if(timeRemaining > 0) {
+        setTimeout(() => {
+            setTimeRemaining(time => time - 1)
+        }, 1000)
+      }
+    }, [timeRemaining])
 
   return (
     <div className="App">
@@ -16,8 +30,8 @@ function App() {
         onChange={handleChange}
         value={text}
       />
-      <h4>Time reminaing: ???</h4>
-      <button>Start</button>
+      <h4>Time reminaing: {timeRemaining}</h4>
+      <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
