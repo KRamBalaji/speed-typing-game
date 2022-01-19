e9db8b3f431cfb54d1b8746ead2a08ec42ea94ae
KRamBalaji <krambalaji27@gmail.com>
2022-01-19 21:25:02 +0530

    created a custom hook and styled the app before deploying

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import useWordGame from "./hooks/useWordGame";
 import './App.css';
 
 function App() {
-  const [text,setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
-  const [isTimeRunning, setIsTimeRunning] = useState(false);
-
-  function handleChange(e) {
-    const {value} = e.target;
-    setText(value);
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ")
-    return wordsArr.filter(word => word !== "").length
-  }
-
-  useEffect(() => {
-    if(isTimeRunning && timeRemaining > 0) {
-        setTimeout(() => {
-            setTimeRemaining(time => time - 1)
-        }, 1000)
-      }
-      else if(timeRemaining === 0) {
-        setIsTimeRunning(false)
-      }
-    }, [timeRemaining, isTimeRunning])
+  const {
+    textBoxRef, 
+    handleChange, 
+    text, 
+    isTimeRunning, 
+    timeRemaining, 
+    startGame, 
+    wordCount
+  } = useWordGame(30)
 
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea 
+      <textarea
+        ref={textBoxRef} 
         onChange={handleChange}
         value={text}
+        disabled={!isTimeRunning}
       />
-      <h4>Time reminaing: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start</button>
-      <h1>Word count: ???</h1>
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame} disabled={isTimeRunning}>Start</button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
